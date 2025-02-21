b1dbd6fec617160b350076f157d83226323776e2
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2025-02-21 16:23:54 -0500

    Added Light Source

---
+++ RayTracer/Objects/LightSource.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace RayTracer.Objects
+{
+    public class LightSource
+    {
+        private Point _position;
+        private Color _color;
+
+        public Point Center { get => _position; }
+        public Color Color { get => _color; }
+
+        public LightSource(Point position, Color color)
+        {
+            _position = position;
+            _color = color;
+        }
+
+        public void Transform(Matrix<double> m)
+        {
+            _position.Transform(m);
+        }
+    }
+}
